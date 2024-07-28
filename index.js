0b201054220fb7965d4f7de1902cf3b42c9bb9d0
Titus kipchirchir <vantiger817@tgmail.com>
2024-07-28 23:54:55 +0300

    complete_lap

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+  function scuberGreetingForFeet(someValue) {
+    if (someValue <= 400) {
+      return "This one is on me!";
+    } else if (someValue > 400 && someValue <= 2000) {
+      return "That will be twenty bucks.";
+    } else if (someValue > 2000 && someValue <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+
+
+function ternaryCheckCity(destination) {
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
